cd4f17fecbb654ce0e0fb40c0933a7e165205d17
mlee3223 <stat2315@gmail.com>
2016-01-10 19:38:37 -0500

    Initial Commit

---
+++ QuickWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickWeather
+//
+//  Created by Michael Lee on 1/10/16.
+//  Copyright © 2016 MichaelLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
